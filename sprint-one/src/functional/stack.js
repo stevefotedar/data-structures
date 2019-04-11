8dd631894082c4aca96ceb28d28ba0735079c39e
suryafotedar <suryafotedar@gmail.com>
2019-04-10 19:08:35 -0500

    finished functionality for functional instanciation of queue and stack! :D

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,3 +1,18 @@
+// someInstance.push:
+//input : string
+//output :
+//Process : Adding the string as a value to the incremented numerical key at the storage object
+
+//someInstance.pop:
+//input :
+// output : string (that we passed it via push) from the TOP OF THE STACK!!!
+//Process : storing the last entered string to a variable. deleting that key[value] pair and returning the value
+
+//someInstance.size
+//input : none
+//output : length of the stack in storage
+//process : Object.keys(storage).length
+
 var Stack = function() {
   var someInstance = {};
 
@@ -6,12 +21,17 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[someInstance.size()] = value;
   };
 
   someInstance.pop = function() {
+    var result = storage[someInstance.size() -1];
+    delete storage[someInstance.size() -1];
+    return result;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
