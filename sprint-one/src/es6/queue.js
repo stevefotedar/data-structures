570b72afac6f56b425cdeca8e2f30c0e7c6ec1d6
suryafotedar <suryafotedar@gmail.com>
2019-04-11 13:24:38 -0500

    Completed es6 instantiation of queue and stack and passed all tests

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,26 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+  }
+
+  enqueue(value){
+    this.storage[this.size()] = value;
+  }
+
+  dequeue(){
+    var results = this.storage["0"];
+    for(var i =0; i < this.size(); i++){
+      this.storage[i] = this.storage[i+1]
+    }
+    delete this.storage[this.size()-1];
+    return results;
+  }
+
+  size(){
+    return Object.keys(this.storage).length;
   }
 
 }
+
+var newQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,22 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  push(value) {
+    this.storage[this.size()] = value;
+  }
+
+  pop() {
+    var result = this.storage[this.size() - 1];
+    delete this.storage[this.size() - 1];
+    return result;
+  }
+
+  size() {
+    return Object.keys(this.storage).length;
+  }
 }
+
+var newStack = new Stack()
