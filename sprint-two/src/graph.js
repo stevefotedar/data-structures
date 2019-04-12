3b3aead8378ad8b90c75d5a66ab01a35cd51f9aa
suryafotedar <suryafotedar@gmail.com>
2019-04-11 20:16:02 -0500

    Finished graph+tree datastructure constructor functions+passed testing.

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -6,30 +6,45 @@ var Graph = function() {
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+  this[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+  return this.hasOwnProperty(node)
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+  for(var i = 0; i < this[node].length; i++){
+    this.removeEdge(this[node][i], node);
+  }
+  delete this[node];
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this[fromNode].includes(toNode);
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this[fromNode].push(toNode);
+  this[toNode].push(fromNode);
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  this[fromNode].splice(this[fromNode].indexOf(toNode),1);
+  this[toNode].splice(this[toNode].indexOf(fromNode),1);
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+  var nodes = Object.keys(this);
+  for (var i = 0; i < nodes.length; i++) {
+    cb(nodes[i]);
+  }
 };
 
 /*
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,19 +1,42 @@
+// A tree is a hierarchical data structure consisting of a node (potentially) with children. The children are trees unto themselves, that is, nodes with (potential) children. For this reason the tree is referred to as a recursive data structure.
+
+// Tree image
+
+// A tree would be great for....
+// Making a family tree.
+
+// A tree class, in functional with shared methods style, with the following properties:
+//   .children property, an array containing a number of subtrees
+//   .addChild() method, takes any value, sets that as the target of a node, and adds that node as a child of the tree
+//   .contains() method, takes any input and returns a boolean reflecting whether it can be found as the value of the target node or any descendant node
+// What is the time complexity of the above functions?
+
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
-treeMethods.contains = function(target) {
+treeMethods.contains = function(target) {  
+  if (this.value === target){
+    return true;
+  }
+  for(var i = 0; i < this.children.length; i++){
+    if(this.children[i].contains(target) === true){
+      return true;
+    }
+  }
+  return false;
 };
 
 
