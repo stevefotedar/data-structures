18e186eda7deaf3cfdff5347ce2cd231e306b1ab
suryafotedar <suryafotedar@gmail.com>
2019-04-12 16:24:22 -0500

    Finished hashTable and completed all tests!!!!!!!!!

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,16 +5,49 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
-HashTable.prototype.insert = function(k, v) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.insert = function(key, value) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (Array.isArray(this._storage.get(index)) === true && JSON.stringify(this._storage.get(index)).includes(key)) {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === key) {
+        this._storage.get(index)[i][1] = value;
+      }
+    }
+  }
+  if (!Array.isArray(this._storage.get(index))) {
+    this._storage.set(index, [[key, value]]);
+  }
+  if (this._storage.get(index) && !JSON.stringify(this._storage.get(index)).includes(key)) {
+    this._storage.get(index).push([key, value]);
+  }
 };
 
-HashTable.prototype.retrieve = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.retrieve = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  if (this._storage.get(index).length === 1) {
+    return this._storage.get(index)[0][1];
+  } else {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === key) {
+        return this._storage.get(index)[i][1];
+      }
+    }
+  }
 };
 
-HashTable.prototype.remove = function(k) {
-  var index = getIndexBelowMaxForKey(k, this._limit);
+HashTable.prototype.remove = function(key) {
+  var index = getIndexBelowMaxForKey(key, this._limit);
+  var newArray = this._storage.get(index);
+  if (this._storage.get(index).length === 1) {
+    newArray.splice(0, 1);
+  } else {
+    for (var i = 0; i < this._storage.get(index).length; i++) {
+      if (this._storage.get(index)[i][0] === key) {
+        newArray.splice(i, 1);
+      }
+    }
+  }
+  this._storage.set(index, newArray);
 };
 
 
