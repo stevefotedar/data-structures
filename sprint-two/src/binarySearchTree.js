3b154327332af7642f5832c6793675f55e65359b
suryafotedar <suryafotedar@gmail.com>
2019-04-13 11:13:55 -0500

    Completed BST and passed all given test!

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,22 +1,40 @@
 var BinarySearchTree = function(value) {
-  var newTree = Object.create(BinarySearchTree.prototype);
+  var newTree = Object.create(binTreeMethods);
 
   newTree.value = value;
   newTree.left = null;
   newTree.right = null;
-
+  return newTree;
 };
 
-BinarySearchTree.prototype.insert = function (value) {
+binTreeMethods = {};
+
+binTreeMethods.insert = function (value) {
   var node;
-  for (var i = this; i.value !== null; i.value > value ? i = i.left : i = i.right) {
+  for (var i = this; i !== null; i.value > value ? i = i.left : i = i.right) {
     node = i;
   }
-  node > value ? node.left = BinarySearchTree(value) : node.right = BinarySearchTree(value);
+  // console.log(node);
+  node.value > value ? node.left = BinarySearchTree(value) : node.right = BinarySearchTree(value);
 };
 
-BinarySearchTree.prototype.contains = function (value) {
-  var node;
+binTreeMethods.contains = function (target) {
+  for (var currentNode = this; currentNode !== null; currentNode.value > target ? currentNode = currentNode.left : currentNode = currentNode.right) {
+    if(currentNode.value === target) {
+      return true;
+    }
+  }
+  return false;
+};
+
+binTreeMethods.depthFirstLog = function (callBack) {
+  callBack(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callBack);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callBack);
+  }
 };
 
 /*
