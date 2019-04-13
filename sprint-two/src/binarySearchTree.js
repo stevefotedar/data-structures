31b3bedbce8dcb1aa753945bf33db903c4a8f399
suryafotedar <suryafotedar@gmail.com>
2019-04-12 19:58:50 -0500

    Significant progress on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,6 +1,23 @@
 var BinarySearchTree = function(value) {
+  var newTree = Object.create(BinarySearchTree.prototype);
+
+  newTree.value = value;
+  newTree.left = null;
+  newTree.right = null;
+
 };
 
+BinarySearchTree.prototype.insert = function (value) {
+  var node;
+  for (var i = this; i.value !== null; i.value > value ? i = i.left : i = i.right) {
+    node = i;
+  }
+  node > value ? node.left = BinarySearchTree(value) : node.right = BinarySearchTree(value);
+};
+
+BinarySearchTree.prototype.contains = function (value) {
+  var node;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
